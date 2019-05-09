484f176b0b0a4def9a443cd979db8de9bc2408ee
littletreepc <47153373+littletreepc@users.noreply.github.com>
2019-05-08 17:10:37 -0700

    Add files via upload

---
+++ Homework_4_1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+letters = [x.strip('') for x in input('Separate value by space ').split()]
+letters.insert(-1,'and')
+letters.append(".")
+
+print(letters)
+
+
+
+
+
+
+    
+    
+   
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
